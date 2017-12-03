ca3260cd71ad9d600131dfa45f9513ac72fc4394
Calum-W <cal7907@hotmail.co.uk>
2017-12-03 17:21:54 +0000

    Adds a test for multiples of an item being added to the order

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -17,5 +17,10 @@ describe Shop do
       shop.add_to_order(1, 1, basket)
       expect(basket).to include ["Beefburger", 5]
     end
+    it "adds multiples of an item to the basket" do
+      basket = []
+      shop.add_to_order(1, 2, basket)
+      expect(basket).to eq [["Beefburger", 5], ["Beefburger", 5]]
+    end
   end
 end
