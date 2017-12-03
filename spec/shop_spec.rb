f0a6a0e8023cafd99b8b269ddc11c7e18bfca531
Calum-W <cal7907@hotmail.co.uk>
2017-12-03 20:16:04 +0000

    Adds gemfiles

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -8,7 +8,7 @@ describe Shop do
   let(:set_time) { Time.new(2017, 12, 4, 2, 2, 2) }
 
   describe "#add_to_order" do
-    it "adds an item to the basket" do
+    it "adds a single item to the basket" do
       basket = []
       shop.add_to_order(1, 1, basket)
       expect(basket).to include ["Beefburger", 5]
